29ffc4e646c8c883939c5aa34952c6f796ad3a62
jsiu <jonsiu826@gmail.com>
2020-12-22 21:07:49 -0800

    adding styling to navbar

--- sahara/src/Header.js
+++ sahara/src/Header.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
     return (
-      <div className="Header">
+      <div className="header">
         <div className="header_logo">SAHARA</div>
-        <div className="Header_search">
+        <div className="header_search">
           <input className="header_search_input" type="text" />
-
-          <div className="header_nav">
-            <div className="header-option"></div>
-            <div className="header-option"></div>
-            <div className="header-option"></div>
+          <SearchIcon className="header_searchIcon"/>
+        </div>
+        <div className="header_nav">
+          <div className="header_option">
+            <span className="header_optionLineOne">Hello Guest</span>
+            <span className="header_optionLineTwo">Sign In</span>
+          </div>
+          <div className="header_option">
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& Orders</span>
+          </div>
+          <div className="header_option">
+            <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+          </div>
+          <div className="header_optionBasket">
+              <ShoppingBasketIcon/>
+              <span className="header_optionLineTwo header_basketCount">0</span>
           </div>
         </div>
       </div>
