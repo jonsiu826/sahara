945f91a3bd7b99a4e0258c98ad5ae09e2b92bcaf
jsiu <jonsiu826@gmail.com>
2020-12-24 11:48:30 -0800

    adding products to home page

--- sahara/src/Home.js
+++ sahara/src/Home.js
@@ -12,10 +12,48 @@ function Home() {
             alt=""
           />
           <div className="home_row">
-            <Product />
+            <Product
+              title="Harry Potter and the Sorcerer's Stone"
+              price={29.99}
+              image="/harry_potter.jpg"
+              rating={4}
+            />
+            <Product
+              title="Green Ranger Funko Pop"
+              price={11.99}
+              image="/green_ranger.jpg"
+              rating={5}
+            />
           </div>
-          <div className="home_row"></div>
-          <div className="home_row"></div>
+          <div className="home_row">
+            <Product
+              title="Apple iPad Pro (12.9-inch, Wi-Fi + Celluler, 256GV)"
+              price={999.0}
+              image="/ipad.jpeg"
+              rating={4}
+            />
+
+            <Product
+              title="Samsung QLED Monitor Curved"
+              price={649.0}
+              image="/samsung_monitor.jpg"
+              rating={4}
+            />
+            <Product
+              title="Wilson Evolution Game Basketball"
+              price={64.99}
+              image="/basketball.jpeg"
+              rating={4}
+            />
+          </div>
+          <div className="home_row">
+            <Product
+              title="All-Clad Brushed D5 Stainless Cookware Set"
+              price={949.95}
+              image="/all_clad.jpg"
+              rating={5}
+            />
+          </div>
         </div>
       </div>
     );
--- sahara/src/Product.js
+++ sahara/src/Product.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './Product.css'
 
-function Product() {
+function Product({title, image,price, rating}) {
     return (
       <div className="product">
         <div className="product_info">
-          <p>Harry Potter and the Sorcerer's Stone</p>
+          <p>{title}</p>
           <p className="product_price">
             <small>$</small>
-            <strong>19.99</strong>
+            <strong>{price}</strong>
           </p>
           <div className="product_rating">
-            <p>⭐</p>
-            <p>⭐</p>
-            <p>⭐</p>
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>⭐</p>
+              ))}
           </div>
         </div>
 
-        <img src="/harry_potter.jpg" alt="image" />
+        <img src={image} alt="image" />
         <button>Add to Basket</button>
       </div>
     );
