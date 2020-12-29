640e631b5c84a6b1cde8b4b4051811d01d9bf86a
jsiu <jonsiu826@gmail.com>
2020-12-29 11:33:10 -0800

    adding login functionality prior to firebase

--- sahara/src/Login.js
+++ sahara/src/Login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from "react-router-dom"
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+      e.preventDefault();
+
+    }
+
+    const register = e => {
+      e.preventDefault();
+
+    }
+    
     return (
       <div className="login">
         <Link to="/">
@@ -12,17 +25,17 @@ function Login() {
           <h1>Sign-in</h1>
           <form>
             <h5>Email</h5>
-            <input type="text" />
+            <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
             <h5>Password</h5>
-            <input type="password" />
-            <button className="login_signin_button">Sign In</button>
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <button type="submit" onClick={signIn} className="login_signin_button">Sign In</button>
           </form>
           <p>
             By signing-in you agree to the Sahara (Amazon Clone) Conditions of Use &
             Sale. Please see our Privacy Notice, our Cookies Notice and our
             Interest-Based Ads Notice.
           </p>
-          <button className="login_register_button">Create your Sahara Account</button>
+          <button onClick={register} className="login_register_button">Create your Sahara Account</button>
         </div>
       </div>
     );
