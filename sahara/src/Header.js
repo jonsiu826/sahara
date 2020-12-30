61a1f3c18380175813c1eb07de077264405f7cb9
jsiu <jonsiu826@gmail.com>
2020-12-30 11:18:26 -0800

    adding signin/signout func to header

--- sahara/src/Header.js
+++ sahara/src/Header.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import { useStateValue } from './StateProvider';
 
 function Header() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket , user}, dispatch] = useStateValue();
 
     return (
       <div className="header">
@@ -22,7 +22,7 @@ function Header() {
           <Link to="/login">
             <div className="header_option">
                 <span className="header_optionLineOne">Hello Guest</span>
-                <span className="header_optionLineTwo">Sign In</span>
+                <span className="header_optionLineTwo">{user ? 'Sign Out' : 'Sign In'} </span>
             </div>
           </Link>
           <div className="header_option">
