ea458e245b6dd9509bbdea7a4116028d06319e47
jsiu <jonsiu826@gmail.com>
2020-12-30 10:18:43 -0800

    implementing user auth

--- sahara/src/Login.js
+++ sahara/src/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase" 
 
 function Login() {
+    const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -13,9 +15,18 @@ function Login() {
 
     const register = e => {
       e.preventDefault();
+      auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+        if (auth) {
+          history.push('/')
+        }
+      })
+      .catch(error => alert(error.message))
 
     }
-    
+
     return (
       <div className="login">
         <Link to="/">
--- sahara/src/firebase.js
+++ sahara/src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDCMWNZeR42M_Km7q276A2itUp4eNh_4L4",
   authDomain: "sahara-7331b.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:1049027595375:web:5d80866f9b6527fa27519a",
   measurementId: "G-193PKBBN8G",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
