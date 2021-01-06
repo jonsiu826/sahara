6020d8d4297e0f51f3a762a237f59c4b20605c2e
jsiu <jonsiu826@gmail.com>
2021-01-05 22:09:32 -0800

    adding stripe func

--- sahara/src/App.js
+++ sahara/src/App.js
@@ -8,8 +8,14 @@ import Checkout from './Checkout'
 import { BrowserRouter as Router, Switch, Route}
 from "react-router-dom"
 import Login from "./Login";
-import { auth } from "./firebase"
+import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51I6UaCGzo5WLS1I6bK2lOPx9WtNTEQjDD4GUBEhquvwAGZJ8GYISDWxhhkRaq6az0oxeIc8rf4WzNbxUAFWOPFOD000lkp3R4d"
+);
 
 function App() {
   const[{}, dispatch] = useStateValue();
@@ -17,7 +23,7 @@ function App() {
   useEffect(() => {
     //will only run once when the app component loads
     auth.onAuthStateChanged(authUser => {
-      console.log("THE USER IS >>>", authUser);
+    
       if (authUser){
         // the user just logged in / the user was logged in 
         dispatch({
@@ -46,7 +52,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
--- sahara/src/Payment.js
+++ sahara/src/Payment.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Payment.css';
 import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
-import { Link } from "react-router-dom"
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
+import { Link } from "react-router-dom";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 function Payment() {
     const [{ basket, user }, dispatch] = useStateValue();
+
+    const stripe = useStripe(null);
+    const elements = useElements(true);
+
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const handleSubmit = e => {
+
+    }
+
+    const handleChange = event => {
+        setDisabled(event.empty);
+        setError((event.error ? event.error.message : ""))
+    }
+
     return (
       <div className="payment">
         <div className="payment_container">
@@ -45,7 +64,23 @@ function Payment() {
                   <h3>Payment Method</h3>
               </div>
               <div className="payment_details"> 
-                    
+                    <form>
+                        <CardElement onChange={handleChange}/>
+                        <div className="payment_priceContainer">
+                                <CurrencyFormat
+                                renderText={(value) => (
+                                    <>
+                                    <h3>Order Total: {value}</h3>
+                                    </>
+                                )}
+                                decimalScale={2}
+                                value={getBasketTotal(basket)} 
+                                displayType={"text"}
+                                thousandSeparator={true}
+                                prefix={"$"}
+                                />
+                        </div>
+                    </form>
               </div>
           </div>
         </div>
