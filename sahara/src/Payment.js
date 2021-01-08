e197a70d19ee6d8869a6936a9b90161de5ed82c1
jsiu <jonsiu826@gmail.com>
2021-01-08 11:56:28 -0800

    adding stripe from backend

--- sahara/src/Payment.js
+++ sahara/src/Payment.js
@@ -35,23 +35,43 @@ function Payment() {
         getClientSecret();
     }, [basket])
 
-    const handleSubmit = async (event) => {
+    console.log('THE SECRET IS >>>', clientSecret)
+
+      const handleSubmit = async (event) => {
+        // do all the fancy stripe stuff...
         event.preventDefault();
         setProcessing(true);
 
-        const payload = await stripe.confirmCardPayment(clientSecret, { 
+        const payload = await stripe
+          .confirmCardPayment(clientSecret, {
             payment_method: {
-                card: elements.getElement(CardElement)
-            }
-        }).then(({paymentIntent}) => {
-            //paymentIntent = payment confirmation
+              card: elements.getElement(CardElement),
+            },
+          })
+          .then(({ paymentIntent }) => {
+            // paymentIntent = payment confirmation
+
+            // db.collection("users")
+            //   .doc(user?.uid)
+            //   .collection("orders")
+            //   .doc(paymentIntent.id)
+            //   .set({
+            //     basket: basket,
+            //     amount: paymentIntent.amount,
+            //     created: paymentIntent.created,
+            //   });
 
             setSucceeded(true);
             setError(null);
-            setProcessing(false)
-            history.replace('/orders')
-        })
-    }
+            setProcessing(false);
+
+            // dispatch({
+            //   type: "EMPTY_BASKET",
+            // });
+
+            history.replace("/orders");
+          });
+      };
 
     const handleChange = event => {
         setDisabled(event.empty);
@@ -61,64 +81,62 @@ function Payment() {
     return (
       <div className="payment">
         <div className="payment_container">
-            <h1>
-                Checkout (
-                    <Link to="/checkout">{basket?.length} items</Link>
-                    )
-            </h1>
+          <h1>
+            Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+          </h1>
           <div className="payment_section">
-            <div className='payment_title'>
-                <h3>Delivery Address</h3>
+            <div className="payment_title">
+              <h3>Delivery Address</h3>
             </div>
             <div className="payment_address">
-                <p>{user?.email}</p>
-                <p>123 React Lane</p>
-                <p>San Francisco, CA</p>
+              <p>{user?.email}</p>
+              <p>123 React Lane</p>
+              <p>San Francisco, CA</p>
             </div>
           </div>
           <div className="payment_section">
-              <div className='payment_title'>
-                  <h3>Review Items and Delivery</h3>
-              </div>
-              <div className='payment_items'>
-                    {basket.map(item => (
-                        <CheckoutProduct 
-                            id={item.id}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}
-                        />
-                    ))}
-              </div>
+            <div className="payment_title">
+              <h3>Review Items and Delivery</h3>
+            </div>
+            <div className="payment_items">
+              {basket.map((item) => (
+                <CheckoutProduct
+                  id={item.id}
+                  title={item.title}
+                  image={item.image}
+                  price={item.price}
+                  rating={item.rating}
+                />
+              ))}
+            </div>
           </div>
           <div className="payment_section">
-              <div className="payment_title">
-                  <h3>Payment Method </h3>
-              </div>
-              <div className="payment_details"> 
-                    <form>
-                        <CardElement onChange={handleChange}/>
-                        <div className="payment_priceContainer">
-                                <CurrencyFormat
-                                renderText={(value) => (
-                                    <>
-                                    <h3>Order Total: {value}</h3>
-                                    </>
-                                )}
-                                decimalScale={2}
-                                value={getBasketTotal(basket)} 
-                                displayType={"text"}
-                                thousandSeparator={true}
-                                prefix={"$"}
-                                />
-                                <button disabled={processing || disabled || succeeded}>
-                                    <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
-                                </button>
-                        </div>
-                        {error && <div>{error}</div>}
-                    </form>
-              </div>
+            <div className="payment_title">
+              <h3>Payment Method </h3>
+            </div>
+            <div className="payment_details">
+              <form onSubmit={handleSubmit}>
+                <CardElement onChange={handleChange} />
+                <div className="payment_priceContainer">
+                  <CurrencyFormat
+                    renderText={(value) => (
+                      <>
+                        <h3>Order Total: {value}</h3>
+                      </>
+                    )}
+                    decimalScale={2}
+                    value={getBasketTotal(basket)}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"$"}
+                  />
+                  <button disabled={processing || disabled || succeeded}>
+                    <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                  </button>
+                </div>
+                {error && <div>{error}</div>}
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- sahara/src/axios.js
+++ sahara/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: '...' // THE API (cloud function) URL
+  baseURL: "http://localhost:5001/sahara-7331b/us-central1/api", // THE API (cloud function) URL
 });
 
 export default instance;
