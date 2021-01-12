2603c48cb6526c827f2f6a73974be7b28ddb6225
jsiu <jonsiu826@gmail.com>
2021-01-11 21:35:38 -0800

    deploying backend

--- sahara/src/axios.js
+++ sahara/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/sahara-7331b/us-central1/api", // THE API (cloud function) URL
+  baseURL: "https://us-central1-sahara-7331b.cloudfunctions.net/api"
+    //"http://localhost:5001/sahara-7331b/us-central1/api", // THE API (cloud function) URL
 });
 
 export default instance;
